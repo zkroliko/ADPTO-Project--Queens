d1b5331e2e4b9c9f6089aec702e311765f827a07
zkroliko <zbkrolikowski@gmail.com>
2016-06-11 17:53:46 +0200

    Included some debugging

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,35 @@
 #include <iostream>
 #include "../include/Loader.h"
 
+#define DEBUG_ENABLED
+
+#ifdef DEBUG_ENABLED
+    #define DEBUG(x) std::cerr << x << endl;
+#else
+    #define DEBUG(x)
+#endif
+
 using namespace std;
 
 int main() {
     int solution_size, board_size;
     /* Getting the problem specification */
     cin >> board_size;
-    cout << "Board size: " << board_size << endl;
+    DEBUG("Board size: " << board_size);
     cin >> solution_size;
-    cout << "Solving solution of size: " << solution_size << endl;
+    DEBUG("Solving solution of size: " << solution_size);
     /* Getting the board */
+    //Allocating
+    int** board_chars = new int*[board_size];
+    for(int i = 0; i < board_size; ++i)
+        board_chars[i] = new int[board_size];
+    DEBUG("Board allocated");
+
+    //Deallocating
+    for(int i = 0; i < board_size; ++i)
+        delete board_chars[i];
+    delete board_chars;
+    DEBUG("Board deallocated");
 //    Loader::load(new char**[]);
     Board board(8);
 
