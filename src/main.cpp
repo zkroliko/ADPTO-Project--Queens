4078735387e1efd803f23f8419b425369599eaa8
zkroliko <zbkrolikowski@gmail.com>
2016-06-28 16:04:06 +0200

    Small addition for future use of main

--- src/main.cpp
+++ src/main.cpp
@@ -29,10 +29,12 @@ int main() {
     if (solver.possible(solution_size)) {
         DEBUG("Solution found");
         for (Move move : *solver.getSolution()) {
-            cout << std::get<0>(std::get<0>(move)->getPosition()) << " " << std::get<1>(std::get<0>(move)->getPosition());
-            cout << " ";
-            cout << std::get<0>(std::get<1>(move)->getPosition()) << " " << std::get<1>(std::get<1>(move)->getPosition());
-            cout << endl;
+            if (std::get<0>(move) != std::get<1>(move)) { // Not the same
+                cout << std::get<0>(std::get<0>(move)->getPosition()) << " " << std::get<1>(std::get<0>(move)->getPosition());
+                cout << " ";
+                cout << std::get<0>(std::get<1>(move)->getPosition()) << " " << std::get<1>(std::get<1>(move)->getPosition());
+                cout << endl;
+            }
         }
     } else {
         DEBUG("Solution not found");
